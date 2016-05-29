c5fe2c078de0a3f65a1e54a3b432e580b24478f0
MJoosens <marino_jo@hotmail.com>
2016-05-30 00:05:35 +0200

    eigen api call OOH YEAH

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ var express = require("express");
 var bodyparser = require("body-parser");
 var app = express();
 
+var request = require ('request');
+
 var Firebase = require("firebase");
 
 app.use(bodyparser.json());
@@ -12,7 +14,26 @@ app.use(function(req, res, next) {
   next();
 });
 
+app.use(bodyparser.urlencoded({ extend: true}));
+
+var port = process.env.PORT || 1337;
+
+var router = express.Router();
+
+router.use(function(req,res,next){
+
+	next();
+})
+router.get('/',function(req,res){
+	res.json({message: "RESPEK BRUU"})
+})
+
+app.use('/api', router);
+
+
 var stripe = require("stripe")(
   "sk_test_BQokikJOvBiI2HlWgH4olfQ2"
 );
-app.listen(3000);
+app.listen(port);
+console.log('API is on this one:' + port);
+
